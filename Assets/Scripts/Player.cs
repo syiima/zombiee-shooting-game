b2b5492810a4069f1a157a920359784cb6e21eae
spacechimp <syimismail@gmail.com>
2022-10-29 16:01:31 +0800

    scripts folder

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    private Vector2 _input;
+
+    public float Speed;
+    public GameObject BulletPrefab;
+    public Transform GunPoint;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.up = (MouseUtils.GetMousePosition2d() - (Vector2)transform.position).normalized;
+        _rigidbody.velocity = _input.normalized * Speed;
+        if(Input.GetButtonDown("Fire1"))
+        { 
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(BulletPrefab, GunPoint.position, transform.rotation);
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+        // FindObjectOfType<TMP_Text>().enabled = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var enemy = other.collider.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            Die();
+        }
+    }
+}
